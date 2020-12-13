499a8b0e5b161bbf2b0ff119b90e8e4df1263527
GaborNagy <gabor.nagy.1985@gmail.com>
2020-12-13 15:22:10 +0100

    "addition function is ready"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,14 @@ package com.company;
 
 public class Main {
 
+    public static int addition(int a, int b) {
+        return a + b;
+
+    }
+
     public static void main(String[] args) {
-	// write your code here
+
+        int calcResAdd = addition(11, 49);
+        System.out.println("11 + 49 = " + calcResAdd);
     }
 }
